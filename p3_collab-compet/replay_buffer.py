f4a380376eff05a74b85835ea8bb64c8dd8ff2f6
Paulo Rodrigues <pauloe_jr@hotmail.com>
2023-07-09 01:01:57 -0300

    style (buffer): improve naming

--- p3_collab-compet/replay_buffer.py
+++ p3_collab-compet/replay_buffer.py
@@ -36,18 +36,18 @@ class ReplayBuffer:
 
         experiences = random.sample(self.memory, k=self.batch_size)
 
-        states_list = [torch.from_numpy(np.vstack([e.states[index] for e in experiences if e is not None])).float(
+        states = [torch.from_numpy(np.vstack([e.states[index] for e in experiences if e is not None])).float(
         ).to(device) for index in range(self.n_agents)]
-        actions_list = [torch.from_numpy(np.vstack([e.actions[index] for e in experiences if e is not None])).float(
-        ).to(device) for index in range(self.n_agents)]
-        next_states_list = [torch.from_numpy(np.vstack([e.next_states[index] for e in experiences if e is not None])).float(
+        actions = [torch.from_numpy(np.vstack([e.actions[index] for e in experiences if e is not None])).float(
         ).to(device) for index in range(self.n_agents)]
         rewards = torch.from_numpy(np.vstack(
             [e.rewards for e in experiences if e is not None])).float().to(device)
+        next_states = [torch.from_numpy(np.vstack([e.next_states[index] for e in experiences if e is not None])).float(
+        ).to(device) for index in range(self.n_agents)]
         dones = torch.from_numpy(np.vstack(
             [e.dones for e in experiences if e is not None]).astype(np.uint8)).float().to(device)
 
-        return (states_list, actions_list, rewards, next_states_list, dones)
+        return (states, actions, rewards, next_states, dones)
 
     def __len__(self):
         """Return the current size of internal memory."""
